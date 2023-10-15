9ffe8894f51d77a7eb7bc2ee1dc63c497b4cdcde
Duong Hong An <themasterclaus@gmail.com>
2023-10-15 22:13:27 +0700

    http api: add headers parameters

--- minevnlib-plugin/src/main/java/net/minevn/libs/Utils.kt
+++ minevnlib-plugin/src/main/java/net/minevn/libs/Utils.kt
@@ -6,7 +6,12 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
-fun http(url: String, method: String, parameters: Map<String, String>?) : String {
+fun http(
+    url: String,
+    method: String,
+    parameters: Map<String, String>? = null,
+    headers: Map<String, String>? = null
+) : String {
     val body = parameters?.map { it.key + "=" + it.value }?.joinToString("&") ?: ""
     val httpsCon = (URL(url).openConnection() as HttpURLConnection).apply {
         doOutput = true
@@ -16,6 +21,9 @@ fun http(url: String, method: String, parameters: Map<String, String>?) : String
         setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
         setRequestProperty("charset", "utf-8")
         setRequestProperty("Content-Length", body.byteInputStream().readAllBytes().size.toString())
+        headers?.forEach { (k, v) ->
+            setRequestProperty(k, v)
+        }
 
         useCaches = false
         connect()
@@ -36,6 +44,14 @@ fun http(url: String, method: String, parameters: Map<String, String>?) : String
     return result.toString()
 }
 
-fun get(url: String, parameters: Map<String, String>? = null) = http(url, "GET", parameters)
+fun get(
+    url: String,
+    parameters: Map<String, String>? = null,
+    headers: Map<String, String>? = null
+) = http(url, "GET", parameters, headers)
 
-fun post(url: String, parameters: Map<String, String>? = null) = http(url, "POST", parameters)
+fun post(
+    url: String,
+    parameters: Map<String, String>? = null,
+    headers: Map<String, String>? = null
+) = http(url, "POST", parameters, headers)
