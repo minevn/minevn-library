40acd38244ba7197ef11e55b0ac1a58d6a93e069
Duong Hong An <rakusvn.dhan@rakus.co.jp>
2023-10-17 16:44:06 +0700

    advanced http api

--- minevnlib-plugin/src/main/java/net/minevn/libs/Utils.kt
+++ minevnlib-plugin/src/main/java/net/minevn/libs/Utils.kt
@@ -5,22 +5,27 @@ import java.io.DataOutputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.streams.asSequence
 
 fun http(
     url: String,
     method: String,
+    contentType: String = "application/x-www-form-urlencoded",
+    body: String? = null,
     parameters: Map<String, String>? = null,
     headers: Map<String, String>? = null
 ) : String {
-    val body = parameters?.map { it.key + "=" + it.value }?.joinToString("&") ?: ""
+    val content = body ?: parameters?.map { it.key + "=" + it.value }?.joinToString("&") { it }
     val httpsCon = (URL(url).openConnection() as HttpURLConnection).apply {
         doOutput = true
         doInput = true
         instanceFollowRedirects = false
         requestMethod = method
-        setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        setRequestProperty("Content-Type", contentType)
         setRequestProperty("charset", "utf-8")
-        setRequestProperty("Content-Length", body.byteInputStream().readAllBytes().size.toString())
+        if (content != null) {
+            setRequestProperty("Content-Length", content.byteInputStream().readAllBytes().size.toString())
+        }
         headers?.forEach { (k, v) ->
             setRequestProperty(k, v)
         }
@@ -29,29 +34,30 @@ fun http(
         connect()
     }
 
-    DataOutputStream(httpsCon.outputStream).apply {
-        write(body.byteInputStream().readAllBytes())
-        close()
+    if (content != null) {
+        DataOutputStream(httpsCon.outputStream).apply {
+            write(content.byteInputStream().readAllBytes())
+            close()
+        }
     }
 
-    val result = StringBuilder()
-    val reader = BufferedReader(InputStreamReader(httpsCon.inputStream))
-    reader.lines().forEach {
-        result.append(it)
-    }
-
+    val result = BufferedReader(InputStreamReader(httpsCon.inputStream)).lines().asSequence().joinToString("\n")
     httpsCon.disconnect()
-    return result.toString()
+    return result
 }
 
 fun get(
     url: String,
+    contentType: String = "application/x-www-form-urlencoded",
+    body: String? = null,
     parameters: Map<String, String>? = null,
-    headers: Map<String, String>? = null
-) = http(url, "GET", parameters, headers)
+    headers: Map<String, String>? = null,
+) = http(url, "GET", contentType, body, parameters, headers)
 
 fun post(
     url: String,
+    contentType: String = "application/x-www-form-urlencoded",
+    body: String? = null,
     parameters: Map<String, String>? = null,
     headers: Map<String, String>? = null
-) = http(url, "POST", parameters, headers)
+) = http(url, "POST", contentType, body, parameters, headers)
