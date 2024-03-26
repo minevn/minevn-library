fe6fd4040689fea8b2c30cf82e43e213913c2c46
Duong Hong An <themasterclaus@gmail.com>
2024-03-26 08:15:54 +0700

    shade thêm file jar không kotlin để tương thích với các plugin kotlin khác

--- minevnlib-plugin/build.gradle.kts
+++ minevnlib-plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("maven-publish")
@@ -28,11 +30,40 @@ publishing {
     }
 }
 
+configurations {
+    create("shadowNoKotlin") {
+        extendsFrom(configurations.getByName("runtimeClasspath"))
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+    }
+}
+
 tasks {
     val jarName = "MineVNLib"
 
-    register("customCopy") {
-        dependsOn(shadowJar)
+    fun ShadowJar.changePackages() {
+        // relocate to avoid conflict
+        relocate("com.zaxxer.hikari", "net.minevn.libs.hikari")
+        relocate("com.fasterxml.jackson", "net.minevn.libs.jackson")
+        relocate("com.google.gson", "net.minevn.libs.gson")
+        exclude("META-INF/versions/21/org/h2/util/Utils21.class")
+    }
+
+    // shadow with kotlin
+    shadowJar {
+        changePackages()
+        archiveFileName.set("$jarName.jar")
+    }
+
+    // shadow without kotlin
+    val noKotlinConfig = configurations.getByName("shadowNoKotlin")
+    val shadowNoKotlin by creating(ShadowJar::class) {
+        configurations = listOf(noKotlinConfig)
+        changePackages()
+        archiveFileName.set("$jarName-no-kotlin.jar")
+    }
+
+    val customCopy by creating(Task::class) {
+        dependsOn(shadowJar, shadowNoKotlin)
 
         val path = project.properties["shadowPath"]
         if (path != null) {
@@ -42,21 +73,16 @@ tasks {
                     from("build/libs/$jarName.jar")
                     into(path)
                 }
+                copy {
+                    from("build/libs/$jarName-no-kotlin.jar")
+                    into(path)
+                }
                 println("Copied")
             }
         }
     }
 
-    shadowJar {
-        // relocate to avoid conflict
-        relocate("com.zaxxer.hikari", "net.minevn.libs.hikari")
-        relocate("com.fasterxml.jackson", "net.minevn.libs.jackson")
-        relocate("com.google.gson", "net.minevn.libs.gson")
-        exclude("META-INF/versions/21/org/h2/util/Utils21.class")
-        archiveFileName.set("$jarName.jar")
-    }
-
     assemble {
-        dependsOn(shadowJar, get("customCopy"))
+        dependsOn(shadowJar, shadowNoKotlin, customCopy)
     }
 }
