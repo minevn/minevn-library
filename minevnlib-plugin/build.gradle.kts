b45d642ca725545d79a7efc7eeec297fca54fdd1
Dương Hồng Ân <themasterclaus@gmail.com>
2025-05-25 16:35:17 +0700

    update anvilgui, support 1.21.5

--- minevnlib-plugin/build.gradle.kts
+++ minevnlib-plugin/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(project(":minevnlib-master"))
     implementation(project(":minevnlib-bukkit"))
     implementation(project(":minevnlib-bungee"))
-    implementation("net.wesjd:anvilgui:1.10.4-SNAPSHOT")
+    implementation("net.wesjd:anvilgui:1.10.5-SNAPSHOT")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
